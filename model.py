c568a58fb821b2f790320f1d59863eabc74dfb40
Sarvesh-Sridhar <qgf510@york.ac.uk>
2026-05-09 17:42:00 +0100

    Built a basic version of the CNN

---
+++ model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNN(nn.module):
+    def __init__(self, num_classes = 2):
+        super(CNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 32, kernal_size = 3, padding = 1)
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size = 3, padding = 1)
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        self.fc1 = nn.Linear(64 * 56 * 56, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+
+        x = x.view(x.size(0), -1)
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
