8949108a620f3a25ffeb8b6572553b2516b2c64d
Sarvesh-Sridhar <qgf510@york.ac.uk>
2026-05-14 15:36:26 +0100

    Added normalisation to train.py

--- train.py
+++ train.py
@@ -9,6 +9,11 @@ from model import CNN
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Device in use:", device)
 
+normalise = transforms.Normalize(
+    mean = [0.485, 0.456, 0.406],
+    std = [0.229, 0.224, 0.225]
+)
+
 train_transform = transforms.Compose([
     transforms.RandomResizedCrop(224, scale = (0.7, 1.0)),
     transforms.RandomHorizontalFlip(),
@@ -18,11 +23,13 @@ train_transform = transforms.Compose([
         saturation = 0.2,
     ),
     transforms.ToTensor(),
+    normalise,
 ])
 
 val_transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
+    normalise,
 ])
 
 train_full_dataset = OxfordIIITPet(
