d6268b2e8238dc9780a53e01627869fafafc626f
Sarvesh-Sridhar <qgf510@york.ac.uk>
2026-05-20 18:08:50 +0100

    Added residual blocks after each bunch of layers and changed dropout value

--- model.py
+++ model.py
@@ -2,6 +2,22 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super (ResidualBlock, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.Conv2d(channels, channels, kernel_size = 3, padding = 1),
+            nn.BatchNorm2d(channels),
+            nn.ReLU(),
+            nn.Conv2d(channels, channels, kernel_size = 3, padding = 1),
+            nn.BatchNorm2d(channels),
+        )
+        self.relu = nn.ReLU() 
+    
+    def forward(self, x):
+        return self.relu(x + self.block(x))
+
 class CNN(nn.Module):
     def __init__(self, num_classes):
         super(CNN, self).__init__()
@@ -11,29 +27,34 @@ class CNN(nn.Module):
         self.conv2 = nn.Conv2d(64, 64, kernel_size = 3, padding = 1)
         self.bn2 = nn.BatchNorm2d(64)
         self.pool1 = nn.MaxPool2d(2)
+        self.resb1 = ResidualBlock(64)
 
         self.conv3 = nn.Conv2d(64, 128, kernel_size = 3, padding = 1)
         self.bn3 = nn.BatchNorm2d(128)
         self.conv4 = nn.Conv2d(128, 128, kernel_size = 3, padding = 1)
         self.bn4 = nn.BatchNorm2d(128)
         self.pool2 = nn.MaxPool2d(2)
+        self.resb2 = ResidualBlock(128)
 
         self.conv5 = nn.Conv2d(128, 256, kernel_size = 3, padding = 1)
         self.bn5 = nn.BatchNorm2d(256)
         self.conv6 = nn.Conv2d(256, 256, kernel_size = 3, padding = 1)
         self.bn6 = nn.BatchNorm2d(256)
         self.pool3 = nn.MaxPool2d(2)
+        self.resb3 = ResidualBlock(256)
 
         self.conv7 = nn.Conv2d(256, 512, kernel_size = 3, padding = 1)
         self.bn7 = nn.BatchNorm2d(512)
         self.conv8 = nn.Conv2d(512, 512, kernel_size = 3, padding = 1)
         self.bn8 = nn.BatchNorm2d(512)
         self.pool4 = nn.MaxPool2d(2)
+        self.resb4 = ResidualBlock(512)
 
-        self.dropout = nn.Dropout(0.15)
+        self.dropout = nn.Dropout(0.05)
         self.gap = nn.AdaptiveAvgPool2d(1)
         self.fc1 = nn.Linear(512, 256)
         self.fc2 = nn.Linear(256, num_classes)
+
 
         self._initialise_weights()
 
@@ -55,19 +76,22 @@ class CNN(nn.Module):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = self.pool1(x)
+        x = self.resb1(x)
 
         x = F.relu(self.bn3(self.conv3(x)))
         x = F.relu(self.bn4(self.conv4(x)))
         x = self.pool2(x)
+        x = self.resb2(x)
 
         x = F.relu(self.bn5(self.conv5(x)))
         x = F.relu(self.bn6(self.conv6(x)))
         x = self.pool3(x)
+        x = self.resb3(x)
 
         x = F.relu(self.bn7(self.conv7(x)))
         x = F.relu(self.bn8(self.conv8(x)))
         x = self.pool4(x)
-
+        x = self.resb4(x)    
 
         x = self.gap(x)
         x = torch.flatten(x, 1)
